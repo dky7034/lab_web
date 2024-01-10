b2e8954457a1240f02847bac9241ba3400daccbc
donggyun <dky7034@naver.com>
2024-01-10 15:25:38 +0900

    commit

--- springboot2/src/main/java/com/itwill/springboot2/domain/Employee.java
+++ springboot2/src/main/java/com/itwill/springboot2/domain/Employee.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
@@ -36,7 +37,7 @@ public class Employee {
     
 //    private Integer deptno;
     
-    @ManyToOne // FK 컬럼에 해당하는 엔터티 객체.
+    @ManyToOne(fetch = FetchType.LAZY) // FK 컬럼에 해당하는 엔터티 객체.
     @JoinColumn(name = "deptno") // EMP 테이블에서 DEPT와 join할 수 있는 컬럼 이름.
     private Department department;
 
