384c9a1fa6afaff5b3e6f87da71612f9891095cb
donggyun <dky7034@naver.com>
2024-01-10 14:02:26 +0900

    commit

--- springboot2/src/main/java/com/itwill/springboot2/web/HomeController.java
+++ springboot2/src/main/java/com/itwill/springboot2/web/HomeController.java
@@ -8,12 +8,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class HomeController {
-	
-	@GetMapping("/")
-	public String home() {
-		log.info("home()");
-		
-		return "index"; // 뷰의 이름: templates/index.html
-	}
-	
+    
+    @GetMapping("/")
+    public String home() {
+        log.info("home()");
+        
+        return "index"; // 뷰의 이름: templates/index.html
+    }
+
 }
